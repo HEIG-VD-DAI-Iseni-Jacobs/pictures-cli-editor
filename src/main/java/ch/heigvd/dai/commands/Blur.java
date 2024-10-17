07316c39d4fb25809f4a5b00d9d73017f733c966
aladin-heig <aladin.iseni@hes-so.ch>
2024-10-17 07:27:12 +0200

    Grey implementation and apply spotless

--- src/main/java/ch/heigvd/dai/commands/Blur.java
+++ src/main/java/ch/heigvd/dai/commands/Blur.java
@@ -1,22 +1,17 @@
 package ch.heigvd.dai.commands;
 
 import ch.heigvd.dai.models.BMPImage;
-import picocli.CommandLine;
-
 import java.util.concurrent.Callable;
+import picocli.CommandLine;
 
 @CommandLine.Command(name = "blur", description = "Blur a picture or part of it")
 public class Blur implements Callable<Integer> {
-  @CommandLine.ParentCommand
-  protected Root parent;
+  @CommandLine.ParentCommand protected Root parent;
 
   @Override
   public Integer call() {
     System.out.println(
-        "Blurring "
-            + parent.getInputPath()
-            + " to a picture located at "
-            + parent.getOutputPath());
+        "Blurring " + parent.getInputPath() + " to a picture located at " + parent.getOutputPath());
 
     BMPImage image = new BMPImage(parent.getInputPath(), parent.getOutputPath());
     image.readImage();
--- src/main/java/ch/heigvd/dai/commands/Grey.java
+++ src/main/java/ch/heigvd/dai/commands/Grey.java
@@ -1,14 +1,13 @@
 package ch.heigvd.dai.commands;
 
 import ch.heigvd.dai.models.BMPImage;
-import picocli.CommandLine;
-
+import ch.heigvd.dai.models.Pixel;
 import java.util.concurrent.Callable;
+import picocli.CommandLine;
 
 @CommandLine.Command(name = "grey", description = "Convert a picture to black and white")
 public class Grey implements Callable<Integer> {
-  @CommandLine.ParentCommand
-  protected Root parent;
+  @CommandLine.ParentCommand protected Root parent;
 
   @Override
   public Integer call() {
@@ -21,8 +20,22 @@ public class Grey implements Callable<Integer> {
 
     BMPImage image = new BMPImage(parent.getInputPath(), parent.getOutputPath());
     image.readImage();
-    // TODO
-    
+    Pixel[][] pixels = image.getImage();
+    /**
+     * Turns each pixel to grey by applying the same coefficient on each color. Formula:
+     * https://chatgpt.com/share/670f8a88-59c4-800a-bda0-a135bed2a099
+     */
+    int grey;
+    for (Pixel[] pixel_line : pixels) {
+      for (Pixel pixel : pixel_line) {
+        grey = (int) (0.299 * pixel.getRed() + 0.587 * pixel.getGreen() + 0.114 * pixel.getBlue());
+        pixel.setRed(grey);
+        pixel.setGreen(grey);
+        pixel.setBlue(grey);
+      }
+    }
+    image.writeImage();
+
     return 0;
   }
 }
